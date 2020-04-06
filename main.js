bd4a5f7937d0ff1f0fe9f9cf6f37b2ab6e0b1dd2
thanachai_swa <thanachai.swa@ascendcorp.com>
2020-04-06 17:11:40 +0700

    add seq + option

--- main.js
+++ main.js
@@ -1,24 +1,67 @@
 var $ = $
 
-function QModal(ids) {
-  this.ids = ids
+function QModal(selectors) {
   this.isBreak = false;
+  this.selectors = []
+  for(var i = 0; i<selectors.length ; i++) {
+    this.add(selectors[i])
+  }
 }
 
 QModal.prototype.start = function() {
-  for(var i = 0; i<this.ids.length ; i++) {
-    if (i == this.ids.length - 1) {
-      break;
-    }
+  var modalOption = this._getModalOption(this.selectors[0].option)
+  $(this.selectors[0].selector).modal(modalOption)
+}
 
-    $("#" + this.ids[i]).on('hidden.bs.modal', function(index){
-      if (this.isBreak) {
-        return;
-      }
-      $('#' + this.ids[index + 1]).modal('show')
-    }.bind(this, i));
+QModal.prototype.add = function(selector) {
+  if (!this.selectors) {
+    this.selectors = []
+  }
+  this.selectors.push(this._getModalObject(selector))
+  console.log(this.selectors)
+  if (this.selectors.length == 1) {
+    return;
   }
 
-  // display
-  $('#' + this.ids[0]).modal('show')
+  var previousObject = this.selectors[this.selectors.length - 2]
+  var addedObject = this.selectors[this.selectors.length - 1]
+  $(previousObject.selector).on('hidden.bs.modal', function(){
+    if (this.isBreak) {
+      return;
+    }
+    var modalOption = this._getModalOption(addedObject.option)
+    $(addedObject.selector).modal(modalOption)
+  }.bind(this))
+}
+
+QModal.prototype._getModalObject = function(selector) {
+  var modalSelector;
+  var option = undefined
+  if (typeof selector == 'object') {
+    modalSelector = selector.selector
+    option = {
+      keyboard: selector.keyboard,
+      backdrop: selector.backdrop,
+      focus: selector.focus
+    }
+  } else {
+    modalSelector = selector
+  }
+
+  return {
+    selector: modalSelector,
+    option: option
+  }
+}
+
+QModal.prototype._getModalOption = function(option) {
+  var modalOption = {
+    show: true
+  }
+  if (option) {
+    modalOption.keyboard = !!option.keyboard ? option.keyboard : true
+    modalOption.backdrop = !!option.backdrop ? option.backdrop : true
+    modalOption.focus = !!option.focus ? option.focus : true
+  }
+  return modalOption
 }
